6146e765feb780a0bd48b8e45990075eaf31f243
connor <cwebdale006@west-mec.org>
2019-12-04 15:27:45 -0700

    working on nav component
    why don't images like me

--- airline-app/src/components/Body.js
+++ airline-app/src/components/Body.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Body extends React.Component {
   render() {
     return (
-        <div class="container">
+        <div className="container">
         
         </div>
     )
--- airline-app/src/components/Navbar.js
+++ airline-app/src/components/Navbar.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
+import Plane from '../airplane.jpeg';
 
 export default class Navbar extends React.Component {
   render() {
     return (
       <>
-      <div class="container">
-        
+      <div className="container" id="one">
+        <div id="logo">
+          {/* <a href="http://localhost:3000/">
+            <Image src={Plane} />
+          </a> */}
+        </div>
+        <div id='links'>
+          {/* links */}
+        </div>
       </div>
       <style>{`
       .container {
         display: grid;
-        grid-template-rows: 100%;
-        grid-template-columns: 
       }
+      
     
-      .row {
-        display: grid-column;
-      }
       `}</style>
       </>
     )
