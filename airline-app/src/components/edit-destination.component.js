51686d7bd41ce64aa7a79047ffae123820e83b11
connor <cwebdale006@west-mec.org>
2020-03-06 14:58:30 -0700

    this is a mess
    i might delete this branch and restart 
    nothing works, less works than before, and i'm just confusing myself at this point
    I need to learn routes for my MongoDB database and Auth0's user management api before i do this is think

--- airline-app/src/components/edit-destination.component.js
+++ airline-app/src/components/edit-destination.component.js
@@ -10,19 +10,12 @@ export default class EditDestination extends Component {
   constructor(props) {
     super(props);
   
-    // this.onChangeUsername = this.onChangeUsername.bind(this);
-    // this.onChangeId = this.onChangeId.bind(this);
-    // this.onChangePassword = this.onChangePassword.bind(this);
     this.onChangeDepartDate = this.onChangeDepartDate.bind(this);
     this.onChangeReturnDate = this.onChangeReturnDate.bind(this);
     this.onChangeAmount = this.onChangeAmount.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
-      username: '',
-      id: '',
-      password: '',
-      passwords: [],
       departDate: new Date(),
       returnDate: new Date(),
       users: []
@@ -43,38 +36,7 @@ export default class EditDestination extends Component {
       .catch(function (error) {
         console.log(error);
       })
-
-      // axios.get('http://localhost:5000/users/')
-      //   .then(response => {
-      //     this.setState({
-      //       users: response.data.map(user => user.username),
-      //       passwords: response.data.map(user => user.password),
-      //       ids: response.data.map(user => user._id)
-      //     });
-      //   })
-      //   .catch((error) => {
-      //     console.log(error);
-      //   })
   }
-
-  
-  // onChangeUsername(e) {
-  //   this.setState({
-  //     username: e.target.value
-  //   });
-  // }
-
-  // onChangeId(e) {
-  //   this.setState({
-  //     id: e.target.id
-  //   });
-  // }
-
-  // onChangePassword(e) {
-  //   this.setState({
-  //     password: e.target.value
-  //   });
-  // }
   
   onChangeDepartDate(date) {
     this.setState({
@@ -116,107 +78,79 @@ export default class EditDestination extends Component {
 
 
   render() {
-    let price = this.state.price;
-
-    // auth0
-    const UserName = () => {
-      const { loading, user } = useAuth0();
-
-      if (loading || !user) {
-        return <div>Loading...</div>
-      }
-
       return (
-        <Fragment>
+        <>
           <div>
-            {user.name}
-          </div>
-        </Fragment>
+          <h3>Edit Flight</h3>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group"> 
+              <label>Booking for:  </label>
+              {/* <UserName /> */}
+            </div>
+            <div className="form-group"> 
+              <label>From: </label>
+              <input  type="text"
+                required
+                className="form-control"
+                value={this.state.from}
+                onChange={this.onChangeFrom}
+                readOnly
+              />
+            </div>
+            <div className="form-group"> 
+              <label>To: </label>
+                <input  type="text"
+                  required
+                  className="form-control"
+                  value={this.state.to}
+                  onChange={this.onChangeTo}
+                  readOnly
+                />
+            </div>
+            <div className="form-group"> 
+              <label>Depart date: </label>
+              <div>
+                <DatePicker 
+                  selected={this.state.departDate}
+                  onChange={this.onChangeDepartDate}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label>Return date: </label>
+              <div>
+                <DatePicker 
+                  selected={this.state.returnDate}
+                  onChange={this.onChangeReturnDate}
+                />
+              </div>
+            </div>
+            <div className="form-group"> 
+              <label>Price: </label>
+                <p id="ticketPrice">${price}</p>
+            </div>
+            <div className="form-group" id="amountInput"> 
+              <label>Amount: </label>
+              <input  type="number"
+                  min="0"
+                  max="20"
+                  required
+                  className="form-control"
+                  value={this.state.amount}
+                  onChange={this.onChangeAmount}
+                  />
+            </div>
+
+            <div className="form-group">
+              <input type="submit" value="Book tickets" className="btn btn-primary" />
+            </div>
+          </form>
+        </div>
+        </>
       )
     }
-
-    return (
-      // <>
-      //   <div>
-      //   <h3>Edit Flight</h3>
-      //   <form onSubmit={this.onSubmit}>
-      //     <div className="form-group"> 
-      //       <label>Booking for:  </label>
-      //       <UserName />
-      //       {/* <select ref="userInput"
-      //           className="form-control"
-      //           value={this.state.username}
-      //           onChange={this.onChangeUsername}>
-      //           {
-      //             this.state.users.map(function(user) {
-      //               return <option 
-      //                 key={user}
-      //                 value={user}>{user}
-      //                 </option>;
-      //             })
-      //           }
-      //       </select> */}
-      //     </div>
-      //     <div className="form-group"> 
-      //       <label>From: </label>
-      //       <input  type="text"
-      //         required
-      //         className="form-control"
-      //         value={this.state.from}
-      //         onChange={this.onChangeFrom}
-      //         readOnly
-      //       />
-      //     </div>
-      //     <div className="form-group"> 
-      //       <label>To: </label>
-      //         <input  type="text"
-      //           required
-      //           className="form-control"
-      //           value={this.state.to}
-      //           onChange={this.onChangeTo}
-      //           readOnly
-      //         />
-      //     </div>
-      //     <div className="form-group"> 
-      //       <label>Depart date: </label>
-      //       <div>
-      //         <DatePicker 
-      //           selected={this.state.departDate}
-      //           onChange={this.onChangeDepartDate}
-      //         />
-      //       </div>
-      //     </div>
-      //     <div className="form-group">
-      //       <label>Return date: </label>
-      //       <div>
-      //         <DatePicker 
-      //           selected={this.state.returnDate}
-      //           onChange={this.onChangeReturnDate}
-      //         />
-      //       </div>
-      //     </div>
-      //     <div className="form-group"> 
-      //       <label>Price: </label>
-      //         <p id="ticketPrice">${price}</p>
-      //     </div>
-      //     <div className="form-group" id="amountInput"> 
-      //       <label>Amount: </label>
-      //       <input  type="number"
-      //           min="0"
-      //           max="20"
-      //           required
-      //           className="form-control"
-      //           value={this.state.amount}
-      //           onChange={this.onChangeAmount}
-      //           />
-      //     </div>
-
-      //     <div className="form-group">
-      //       <input type="submit" value="Book tickets" className="btn btn-primary" />
-      //     </div>
-      //   </form>
-      // </div>
-      // </>
-    )
   }
-}
+
+    // return (
+      
+    // )
